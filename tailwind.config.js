ff90140344b393ec4530287aa860410b0ca23f48
mphuc04 <minhphuctn04@gmail.com>
2024-08-20 22:55:00 +0700

    Upload banner solution

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'menu': 'rgb(254 96 25)',
+        'body': '#fafafa'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
